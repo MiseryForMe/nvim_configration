49a0698a17d0a38b5da7c1c716e1f04a0770c405
MiseryForMe <1535640262@qq.com>
2020-07-21 10:50:17 +0800

    注释了sudo apt-get install -y python-software-properties

--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -16,7 +16,7 @@ echo "${BLUE} neovim zsh tmux ctags installation start"
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common
-sudo apt-get install -y python-software-properties 
+#sudo apt-get install -y python-software-properties 
 sudo apt-add-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
